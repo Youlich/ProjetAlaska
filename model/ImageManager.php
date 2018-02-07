827417efbfc012a40617801471251c0ab04c3184
Youlich <jutatibouet@yahoo.fr>
2018-02-07 17:43:08 +0100

    ajout image, lien  et quelques corrections

---
+++ model/ImageManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jutat
+ * Date: 06/02/2018
+ * Time: 16:44
+ */
+
+namespace model;
+
+
+class Image
+{
+
+}
---
+++ view/frontend/lastChapter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jutat
+ * Date: 07/02/2018
+ * Time: 14:00
+ */
