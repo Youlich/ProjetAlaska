405dff7b5e89e9d63adebee8bd4d47a6032dc1d3
saysa <sbo@conamed.de>
2018-02-08 09:37:09 +0100

    translate the controler into a Class

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-namespace model;
-require_once ('controler/frontend.php');
+
+require_once('controler/autoload.php'); // Chargement des class
+
+$frontend = new \controler\Frontend();
 
 try {
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'listPosts') { // c'est l'action par défaut , la fonction qui affiche tous les posts et qui est détaillée dans le frontend.php
-            listPosts();
+            $frontend->listPosts();
         }
         elseif ($_GET['action'] == 'post') { // action qui se réalise quand on clique sur le lien "commentaires du post"
             if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post(); // fonction post de frontend.php
+                $frontend->post(); // fonction post de frontend.php
             }
             else {
                 throw new Exception('Aucun identifiant de billet envoyé');
@@ -18,7 +20,7 @@ try {
         elseif ($_GET['action'] == 'addComment') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
                 if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                    $frontend->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
                 }
                 else {
                     throw new Exception('Tous les champs ne sont pas remplis !');
@@ -29,7 +31,7 @@ try {
             }
         } elseif($_GET['action'] == 'Comment') { // si on clique sur le lien modifier du commentaire
             if (isset($_GET['numComm']) && $_GET['numComm'] > 0) {
-                comment(); // fonction comment utilisée
+                $frontend->comment(); // fonction comment utilisée
             }
             else {
                 throw new Exception('Aucun identifiant de commentaire envoyé');
@@ -38,7 +40,7 @@ try {
              elseif ($_GET['action'] == 'ModifComment') {
             if (isset($_GET['numComm']) && $_GET['numComm'] > 0) {
                 if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    ModifComment();
+                    $frontend->ModifComment();
                 }
                 else {
                     throw new Exception('Tous les champs ne sont pas remplis !');
@@ -51,7 +53,7 @@ try {
         }
     }
     else {
-        listPosts(); // fonction par défaut détaillée dans frontend.php
+        $frontend->listPosts(); // fonction par défaut détaillée dans frontend.php
     }
 }
 catch(Exception $e) {
