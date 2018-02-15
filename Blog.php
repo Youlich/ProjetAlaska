1f11f4097cc188cb93757f0c62b7cf3c80daebf4
saysa <sbo@conamed.de>
2018-02-15 10:08:22 +0100

    nouvelle enoncé

--- Blog.php
+++ Blog.php
@@ -4,6 +4,13 @@ Autoload::register(); // j'appelle la fonction register de ma class Autoload
 
 $frontend = new \controler\Frontend();
 
+// on a plusieurs routes : par ex : listPosts, addComment, ect qui sont des instance de Route
+
+// il faut un Routeur - en anglais Router
+// faire une classe Router, il a une methode diriger() en fonction de la route on execute $frontend->listPosts(); ou $frontend->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+// il a un attribute $routes c'est un tableau d'instances de Route.
+
+// faire une classe Route
 
 try {
     if (isset($_GET['action'])) {
